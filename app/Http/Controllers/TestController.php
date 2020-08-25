93e11435213135bac7b03765dfbfa28489b3ea3a
Corentin RICHARD-VITTON <corenbla@gmail.com>
2020-08-25 15:12:58 +0200

    usePhpMessDetector method

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Services\CodeParser;
+use PHPMD\RuleSetFactory;
 
 class TestController extends Controller
 {
-    public function test(CodeParser $codeParser)
+    public function test(CodeParser $codeParser): string
     {
+        $ruleSetFactory = new RuleSetFactory();
         $project = new Project('ClairiereEtCanopee', 'https://github.com/Corenbla/ClairiereEtCanopee.git');
 
-        return $codeParser->usePhpCsFixer($project, true);
+        return $codeParser->usePhpMessDetector($project, $ruleSetFactory->listAvailableRuleSets());
     }
 }
--- app/Services/CodeParser.php
+++ app/Services/CodeParser.php
@@ -8,11 +8,26 @@ class CodeParser
 {
     public function usePhpCsFixer(Project $project, $withDryRun = false): string
     {
-        $command = sprintf('%s/vendor/bin/php-cs-fixer', base_path(), );
+        $command = sprintf('%s/vendor/bin/php-cs-fixer', base_path());
         $arguments = sprintf(' fix %s/app/Repositories/%s/ -vvv %s --using-cache=false --format=json',
             storage_path(),
             escapeshellcmd($project->getName()),
             $withDryRun ? '--dry-run' : '',
+        );
+
+        return $this->doExec($command, $arguments);
+    }
+
+    /**
+     * @param string[] $ruleSets every RuleSets available can be fetched with PHPMD\RuleSetFactory::listAvailableRuleSets()
+     */
+    public function usePhpMessDetector(Project $project, array $ruleSets): string
+    {
+        $command = sprintf('%s/vendor/bin/phpmd', base_path(),);
+        $arguments = sprintf(' %s/app/Repositories/%s/ json %s',
+            storage_path(),
+            escapeshellcmd($project->getName()),
+            implode(',', $ruleSets),
         );
 
         return $this->doExec($command, $arguments);
@@ -24,9 +39,8 @@ class CodeParser
             $command .= '.bat';
         }
 
-
         exec($command . $arguments, $output);
 
-        return $output[0];
+        return implode($output) ?? json_encode('No output');
     }
 }
