e8153c82cb8d18b4a705806d0e6f4caf7c2b58ca
MelissandeH <melissandehuet@outlook.fr>
2020-08-25 16:00:43 +0200

    Created front componants VueJS WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,15 @@
-import ExampleComponent from './components/Example/ExampleComponent';
+import Vue from 'vue';
+import 'bootstrap';
 
-    /**
+/**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+Vue.component('example-component', require('./components/Example/ExampleComponent.vue').default);
+Vue.component('header-component', require('./components/header/HeaderComponent.vue').default);
 
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-component', {
-  props: ['content'],
-  template: require('./components/Example/ExampleComponent.vue').default
-})
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -32,5 +17,7 @@ Vue.component('example-component', {
  */
 const app = new Vue({
     el: '#app',
-
+    data: {
+      message: 'Hello hello hello'
+  }
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,11 @@
 @section('content')
 
     <div id="app">
+    <header-component></header-component>
+    <h1>Hello world</h1>
+        <p>@{{ message }}</p>
         <example-component></example-component>
+        @yield('content')
     </div>
 
 @endsection
