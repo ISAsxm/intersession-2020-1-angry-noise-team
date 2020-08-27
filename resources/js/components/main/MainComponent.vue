515d43aec3b8fb4873b19e89069631607c413537
MelissandeH <melissandehuet@outlook.fr>
2020-08-27 09:46:13 +0200

    Added different font weights to Manrope public-ressources

--- resources/js/components/main/MainComponent.vue
+++ resources/js/components/main/MainComponent.vue
@@ -1,35 +1,31 @@
 <style>
 .stylemain {
-  width:100%;
+  width: 100%;
 }
 .mainBody {
-  background-color:white;
-  min-height:400px;
+  background-color: white;
+  min-height: 400px;
 }
 </style>
 <template>
-<main>
-  <img class="stylemain" :src="'images/stylemain.png'">
-  <div class="container-fluid mainBody">
-    <section id="resume">
-      <div class="col-lg-12">
+  <main>
+    <img class="stylemain" :src="'images/stylemain.png'" />
+    <div class="container-fluid mainBody">
+      <section id="resume">
         <h2 class="text-light">Présentation de Code Laïka</h2>
-      </div>
-    </section>
-    <section id="funding">
-      <h2 class="text-light">Go Funding</h2>
-    </section>
-    <section id="tool">
-      <h2 class="text-light">Tool</h2>
-    </section>
-  </div>
-</main>
-
+      </section>
+      <section id="funding">
+        <h2 class="text-light">Go Funding</h2>
+      </section>
+      <section id="tool">
+        <h2 class="text-light">Tool</h2>
+      </section>
+    </div>
+  </main>
 </template>
 
 <script>
-
 export default {
-    name: 'main-component'
-}
+  name: "main-component"
+};
 </script>
