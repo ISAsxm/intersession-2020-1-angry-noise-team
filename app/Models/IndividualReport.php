8c21f4a3cac9878235f3af66b3fb285dd3c118bf
MelissandeH <melissandehuet@outlook.fr>
2020-09-03 11:28:56 +0200

    Added ErrorMsg

--- app/Models/IndividualReport.php
+++ app/Models/IndividualReport.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use JsonException;
+use LogicException;
+
 class IndividualReport
 {
     public const PHP_CS_FIXER = 'PhpCsFixer';
@@ -18,9 +21,14 @@ class IndividualReport
 
     /**
      * @throws \JsonException
+     * @throws \LogicException when $json has no output.
      */
     public function __construct(string $json, string $name)
     {
+        if ($json === 'No output') {
+            throw new LogicException("Json was a string with no output, please verify if the output is correct.");
+        }
+
         $this->name = $name;
         $this->decodedJson = json_decode($json, true);
         $this->reportData = new ReportData($this);
